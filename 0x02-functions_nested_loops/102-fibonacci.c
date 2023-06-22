99d20380ab3348aab8701fbf688b99be7df4e4bd
Esraa258 <esraa.tarek3690@gmail.com>
2023-06-21 20:04:07 -0700

    My first commit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,10 +9,12 @@
  *Return: Always 0 (success)
 */
 
-int main(void)
+int main(void);
 {
 	int count:
+
 	unsigned long fib1 = 0, fib2 = 1, sum;
+
 	for (count = 0; count < 50; count++)
 	{
 		sum = fib1 + fib2;
