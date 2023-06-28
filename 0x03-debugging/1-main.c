027c8fa8c12cfc279e8c2b4141152b1e11fa49d8
Esraa258 <esraa.tarek3690@gmail.com>
2023-06-27 23:29:44 -0700

    My first commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,17 +12,13 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
+
 	/*while (i < 10)*/
 	/*{*/
-
-	/*putchar(i);*/
+	/*     putchar(i);*/
 	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
-
-	/*putchar(i);*/
-	/*}*/
-	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
