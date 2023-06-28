c273a80b837e83317c6bb4c0c15837040a5890df
Esraa258 <esraa.tarek3690@gmail.com>
2023-06-27 23:10:34 -0700

    My first commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -10,12 +10,8 @@ int main(void)
 	int i;
 
 	i = 98 % 2;
-<<<<<<< HEAD
-	positive_or_negative(i);
-
-=======
 
 	positive_or_negative(i);
->>>>>>> 7818bfbc62e7496b0047e9bfe68eeb92c50a9b56
+
 	return (0);
 }
