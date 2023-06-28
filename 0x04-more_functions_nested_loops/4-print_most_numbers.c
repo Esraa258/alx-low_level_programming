d709d4546aa2400e46e7286ff46650c9bcb0adc9
Esraa258 <esraa.tarek3690@gmail.com>
2023-06-28 02:15:44 -0700

    My first commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,8 +2,8 @@
 
 /**
  * print_most_numbers - print 0 - 9 apart
- *                 from 2 and 4 and you can
- *                 only use _putchar twice
+ *                from 2 and 4 and you can
+ *                only use _putchar twice
  * Return: Always 0 (success)
 */
 
@@ -14,8 +14,8 @@ void print_most_numbers(void)
 	for (num = 0; num <= 9; num++)
 	{
 		if (num == 2 || num == 4)
-			coninue:
-		_putchar(num + 48);
+			continue:
+		_putchar(num + 4);
 	}
 	_putchar('\n');
 }
