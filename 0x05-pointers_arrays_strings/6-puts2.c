d9029ac06b627df6b7669c2b74d07df7562926ad
Esraa258 <esraa.tarek3690@gmail.com>
2023-07-01 19:52:34 -0700

    My first commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
  * puts2 - prints every character of a string
  *
  * @str: string parameter input
@@ -12,10 +12,10 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; ++i);
+	for (i = 0; str[i] != '\0'; ++i)
 	{
 		if (i % 2 == 0)
 			_putchar(str[i]);
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
