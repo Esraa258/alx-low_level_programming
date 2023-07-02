19a7e6134f6c33a9a347dfa5ba539bf0f34aa328
Esraa258 <esraa.tarek3690@gmail.com>
2023-07-01 17:33:08 -0700

    My first commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,10 +1,4 @@
-#include <stdio>
-
-/**
- * main - entry point
- *
- * Return: Always .
-*/
+#include <stdio.h>
 
 int main(void)
 {
@@ -14,7 +8,7 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/**
+	/*
 	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
@@ -24,6 +18,6 @@ int main(void)
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2];
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
