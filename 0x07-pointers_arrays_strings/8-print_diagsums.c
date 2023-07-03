ce212dc35a846e882de21bb41867d5e855c39355
Esraa258 <esraa.tarek3690@gmail.com>
2023-07-03 12:55:59 -0700

    My first commit

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,19 +3,19 @@
 
 /**
  * print_diagsums - description
- * @arr: 2nd array of int type.
+ * @b: 2nd array of int type.
  * @size: size of array (square)
 */
 
-void print_diagsums(int *arr, int size)
+void print_diagsums(int *b, int size)
 {
 	int itr, s1 = 0, s2 = 0
 
 	for (itr = 0; itr < size; itr++)
 	{
-		s1 += arr[itr];
-		s2 += arr[size - itr - 1];
-		arr += size;
+		s1 += b[itr];
+		s2 += b[size - itr - 1];
+		b += size;
 
 	}
 	printf("%d' "' s1);
