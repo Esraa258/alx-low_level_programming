fe05771ebd8c352e7b54688e7b874992bfc63885
Esraa258 <esraa.tarek3690@gmail.com>
2023-07-03 04:31:17 -0700

    My first commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -38,7 +38,7 @@ int main(void)
 	unsigned long f1 = 1, f2 = 2, sum,
 		      max = 100000000, f1o = 0, f2o = 0, sumo = 0;
 
-	for (count = 1; count <= 98; count++)
+	for (count = 0; count < 98; count++)
 	{
 		if (f1o > 0)
 			printf("%lu", f1o);
@@ -59,7 +59,7 @@ int main(void)
 		f2 = sum;
 		f2o = sumo;
 		if (count != 98)
-			printf(" , ");
+			printf(", ");
 		else
 			printf("\n");
 	}
