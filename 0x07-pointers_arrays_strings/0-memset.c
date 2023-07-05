7f5185e995e887731b4e56318e6c830f614b7e56
Esraa258 <esraa.tarek3690@gmail.com>
2023-07-04 20:08:03 -0700

    My first commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,20 +2,20 @@
 
 /**
  * *_memset - fills memory with a constant byte.
- * @p: pointer to put the constant
- * @c: constant
+ * @s: pointer to put the constant
+ * @b: constant
  * @n: max bytes to use
- * Return: pointer p
+ * Return: pointer s
  */
 
-char *_memset(char *p, char c, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int itr;
 
-	for (itr = 0; itr < n; itr++)
+	for (itr = 0; n > 0; itr++, n--)
 	{
-		*(p + itr) = c;
+		s[itr] = b;
 	}
 
-	return (p);
+	return (s);
 }
