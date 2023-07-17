92d4921e604b9a67c4149cc2cb6109a61ec539d3
Esraa258 <esraa.tarek3690@gmail.com>
2023-07-17 04:40:30 -0700

    My first commit

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints a dog
+ * @d: the dog to print
+ *
+ * Return: void
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		print("Name: %5\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
+}
