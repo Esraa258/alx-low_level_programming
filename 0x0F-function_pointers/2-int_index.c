0fc1647c225e7dd9dd7b6761db89191fda587455
Esraa258 <esraa.tarek3690@gmail.com>
2023-07-19 19:01:00 -0700

    My first commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,16 +9,18 @@
  * Return: the integer index
 */
 
-int int_index(index *array, int size, int (*cmp)(int))
+int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0;
+	int i;
 
-	if (array && size && cmp)
-		while (i < size)
-		{
-			if (cmp(array[i])
-				return (i);
-			i++;
-			}
+	if (size <= 0 || !array || !cmp)
 		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+
+	return (-1);
 }
